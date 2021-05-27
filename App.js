cfafcc62d6293769e8dfebd088079ae0ec8c2699
Joaquin Claren <joaquin.claren@gmail.com>
2021-05-27 17:14:17 -0400

    Separa pantallas

--- App.js
+++ App.js
@@ -1,58 +1,23 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-import React, {useEffect, useState} from 'react';
-import {StyleSheet, Text, View, ActivityIndicator} from 'react-native';
 import {createAppContainer} from 'react-navigation';
 import {createStackNavigator} from 'react-navigation-stack';
-import axios from 'axios';
-
-const HomeScreen = ({navigation}) => {
-  const [loading, setLoading] = useState(true);
-  const [meals, setMeals] = useState([]);
-
-  const fetchMeals = async () => {
-    axios.get('https://mindicador.cl/api').then(function (response) {
-      console.log(response.data);
-      setMeals(response.data);
-    });
-    setLoading(false);
-  };
-
-  useEffect(() => {
-    fetchMeals();
-  }, []);
-
-  return (
-    <View>
-      <ActivityIndicator />
-    </View>
-  );
-};
-
-const DetalleScreen = ({navigation}) => {
-  const name = navigation.getParam('name');
-  return (
-    <View>
-      <Text>Alegale {name}</Text>
-    </View>
-  );
-};
+import IndicatorsScreen from './screens/Indicators';
+import Modal from './screens/Modal';
 
 const AppNavigator = createStackNavigator(
   {
-    Home: {
-      screen: HomeScreen,
-    },
-    Detalle: {
-      screen: DetalleScreen,
+    Indicators: {
+      screen: IndicatorsScreen,
     },
   },
-  {initialRouteName: 'Home'},
+  {initialRouteName: 'Indicators'},
 );
 
-export default createAppContainer(AppNavigator);
+const RootStack = createStackNavigator(
+  {
+    Main: AppNavigator,
+    Modal: Modal,
+  },
+  {mode: 'modal', headerMode: 'none'},
+);
+
+export default createAppContainer(RootStack);
