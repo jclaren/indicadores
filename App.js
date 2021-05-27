87f31b69ca1c0b67ad1d627ec0449afe00068dc8
Joaquin Claren <joaquin.claren@gmail.com>
2021-05-27 15:05:03 -0400

    Agrega pantalla de detalle

--- App.js
+++ App.js
@@ -6,22 +6,36 @@
  * @flow strict-local
  */
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native'
-import {createAppContainer} from 'react-navigation'
-import { createStackNavigator } from 'react-navigation-stack'
+import {StyleSheet, Text, View} from 'react-native';
+import {createAppContainer} from 'react-navigation';
+import {createStackNavigator} from 'react-navigation-stack';
 
 const HomeScreen = () => {
   return (
     <View>
-      <Text></Text>
+      <Text>Vamos que de puede!</Text>
     </View>
-  )
-}
+  );
+};
 
-const AppNavigator = createStackNavigator({
-  Home: {
-    screen: HomeScreen
-  }
-})
+const DetalleScreen = () => {
+  return (
+    <View>
+      <Text>Alegale</Text>
+    </View>
+  );
+};
 
-export default createAppContainer(AppNavigator)
+const AppNavigator = createStackNavigator(
+  {
+    Home: {
+      screen: HomeScreen,
+    },
+    Detalle: {
+      screen: DetalleScreen,
+    },
+  },
+  {initialRouteName: 'Detalle'},
+);
+
+export default createAppContainer(AppNavigator);
