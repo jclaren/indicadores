3ba61d6a4f1b66708d1785d85a49ed22b9538cd6
Joaquin Claren <joaquin.claren@gmail.com>
2021-05-27 17:48:10 -0400

    Implementa flatlist

--- screens/Indicators.js
+++ screens/Indicators.js
@@ -1,10 +1,37 @@
 import React from 'react';
-import {View, Text} from 'react-native';
+import {View, StyleSheet, FlatList} from 'react-native';
+import ListItem from '../components/ListItem';
 
-export default () => {
-    return (
-        <View>
-            <Text>Soy Meals</Text>
-        </View>
-    )
-}
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'flex-start',
+    justifyContent: 'flex-start',
+  },
+  list: {
+    alignSelf: 'stretch',
+  },
+});
+
+const data = {_id: 1, name: 'Churrasco', desc: 'Plato tipico'};
+
+const Indicators = ({navigation}) => {
+  return (
+    <View style={styles.container}>
+      <FlatList
+        style={styles.list}
+        data={data}
+        keyExtractor={x => x._id}
+        renderItem={({item}) => (
+          <ListItem
+            onPress={() => navigation.navigate('modal', {_id: item._id})}
+            name={item.name}
+          />
+        )}
+      />
+    </View>
+  );
+};
+
+export default Indicators;
