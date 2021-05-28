5f410d263d29954afba1e54148ddbf629032b418
Joaquin Claren <joaquin.claren@gmail.com>
2021-05-28 11:09:40 -0400

    Estilos detalle

--- screens/Detail.js
+++ screens/Detail.js
@@ -1,21 +1,19 @@
 import React from 'react';
 import {View, StyleSheet, Text} from 'react-native';
-import axios from 'axios';
 import Chart from '../components/Chart';
+import date from '../components/Date';
 
 const Detail = ({navigation}) => {
   const indicator = navigation.getParam('indicator');
-  console.log('******* Pantalla nueva 2 ********');
-  console.log(indicator);
-
   return (
-      <View style={styles.container}>
-        <Text>{indicator.valor}</Text>
-        <Text>{indicator.nombre}</Text>
-        <Text>{indicator.fecha}</Text>
-        <Text>{indicator.unidad_medida}</Text>
-        <Chart></Chart>
-      </View>      
+    <View style={styles.container}>
+      <Text style={styles.title}>${indicator.valor}</Text>
+      <Text style={styles.info}>Nombre: {indicator.nombre}</Text>
+      <Text style={styles.info}>Fecha: {date(indicator.fecha)}</Text>
+      <Text style={styles.info}>
+        Unidad de medida: {indicator.unidad_medida}
+      </Text>
+    </View>
   );
 };
 
@@ -25,7 +23,16 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'stretch',
+    alignItems: 'center',
     justifyContent: 'flex-start',
+  },
+  title: {
+    color: 'blue',
+    fontSize: 30,
+    padding: 10,
+  },
+  info: {
+    fontSize: 16,
+    padding: 10,
   },
 });
